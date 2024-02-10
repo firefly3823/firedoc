bcfdd586328365bd112167b056075091851053d0
firefly3823 <stevinsaji3823@gmail.com>
2024-02-10 17:54:02 +0530

    card styled

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -50,20 +50,34 @@ function LandingPage() {
         <>
             <div className='d-flex flex-column  justify-content-center' style={{ minHeight: "70vh" }}>
                 <div className='d-flex justify-content-around flex-wrap'>
-                    {allDoc && allDoc.map((doc) => (
+                    {allDoc && allDoc.map((doc,index) => (
                         <div className='mt-4'>
-                            <Card style={{ height: '220px', width: '18rem' }} className='bg-light'>
+                            <Card style={{ height: '220px', width: '18rem' }} key={index+1} className='bg-light'>
                                 <Card.Body>
                                     <div className='d-flex justify-content-end align-items-center'>
                                         <Link className='text-success' to={`/updatedocument/${doc?.id}`} style={{ textDecoration: 'none', color: 'black' }}>
-                                            <i className="fa-solid fa-file-pen fa-2x"></i>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-edit" width={35} height={35} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+                                                <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+                                                <path d="M16 5l3 3" />
+                                            </svg>
                                         </Link>
                                     </div>
                                     <Card.Title className='mt-3 fw-bold '>{doc?.title}</Card.Title>
                                     <Card.Text>
                                         {doc?.note.slice(0, 60)}
                                     </Card.Text>
-                                    <div className='d-flex justify-content-end text-danger' style={{ cursor: 'pointer', marginTop: doc?.note !== "" ? '0px' : '65px', marginRight: '8px' }} ><i className="fa-solid fa-trash fa-2x"></i></div>
+                                    <div className='d-flex justify-content-end text-danger' style={{ cursor: 'pointer', marginTop: doc?.note !== "" ? '0px' : '65px', marginRight: '8px' }} >
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width={35} height={35} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M4 7l16 0" />
+                                            <path d="M10 11l0 6" />
+                                            <path d="M14 11l0 6" />
+                                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                        </svg>
+                                        </div>
 
                                 </Card.Body>
                             </Card>
